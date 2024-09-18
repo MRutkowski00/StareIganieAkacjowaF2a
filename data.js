ab7da980e6128958035ce7394691857964d0e189
MRutkowski00 <m.rutkowski.archviz@gmail.com>
2024-09-18 20:07:44 +0200

    Upload

---
+++ data.js
@@ -0,0 +1,507 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-komunikacja",
+      "name": "Komunikacja",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.21472345481923583,
+          "pitch": 0.057864110812994696,
+          "rotation": 0,
+          "target": "4-pokj"
+        },
+        {
+          "yaw": 0.37709237969552234,
+          "pitch": 0.05493837057101558,
+          "rotation": 0,
+          "target": "3-wc"
+        },
+        {
+          "yaw": 2.1701285689049623,
+          "pitch": 0.059320955739210035,
+          "rotation": 0,
+          "target": "1-salon"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.30402338737195755,
+          "pitch": 0.05393084350327548,
+          "title": "Wiatrołap",
+          "text": "dostęp do pomieszczenia technicznego"
+        }
+      ]
+    },
+    {
+      "id": "1-salon",
+      "name": "Salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.552847669485848,
+          "pitch": 0.06465238274862983,
+          "rotation": 0,
+          "target": "0-komunikacja"
+        },
+        {
+          "yaw": 0.594685354783131,
+          "pitch": 0.0595755776553375,
+          "rotation": 0,
+          "target": "2-aneks-kuchenny"
+        },
+        {
+          "yaw": 2.032387956813115,
+          "pitch": 0.06013009566643035,
+          "rotation": 0,
+          "target": "5-komunikacja---pitro"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-aneks-kuchenny",
+      "name": "Aneks kuchenny",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8712569324533899,
+          "pitch": 0.0799988397511342,
+          "rotation": 0,
+          "target": "1-salon"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0075388066686664,
+          "pitch": 0.07213435577367555,
+          "title": "Spiżarnia",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-wc",
+      "name": "WC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6228039179144238,
+          "pitch": 0.038218854274838066,
+          "rotation": 0,
+          "target": "0-komunikacja"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-pokj",
+      "name": "Pokój",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.83043735115357,
+          "pitch": 0.036121341633215565,
+          "rotation": 0,
+          "target": "0-komunikacja"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7155595857183812,
+          "pitch": 0.07941136190015996,
+          "title": "Garderoba",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "5-komunikacja---pitro",
+      "name": "Komunikacja - piętro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.663546311244632,
+          "pitch": 0.01215561920002095,
+          "rotation": 0,
+          "target": "6-biuro"
+        },
+        {
+          "yaw": -0.420755764979031,
+          "pitch": 7.670863055864174e-9,
+          "rotation": 0,
+          "target": "7-azienka"
+        },
+        {
+          "yaw": 2.7503520865055258,
+          "pitch": 0.03692114157625781,
+          "rotation": 0,
+          "target": "1-salon"
+        },
+        {
+          "yaw": 2.9709975774160995,
+          "pitch": 0.03235585499658988,
+          "rotation": 0,
+          "target": "9-pokj---pitro"
+        },
+        {
+          "yaw": -3.0931701836675956,
+          "pitch": 0.030103417263001475,
+          "rotation": 0,
+          "target": "8-pokj---pitro"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.881251789467459,
+          "pitch": 0.04340945571245314,
+          "title": "Garderoba",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "6-biuro",
+      "name": "Biuro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0961152172306114,
+          "pitch": 0.005513095978036731,
+          "rotation": 0,
+          "target": "5-komunikacja---pitro"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-azienka",
+      "name": "Łazienka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.006779450582899,
+          "pitch": 0.013153126226500333,
+          "rotation": 0,
+          "target": "5-komunikacja---pitro"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-pokj---pitro",
+      "name": "Pokój - piętro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3974728269731056,
+          "pitch": 0.010963525607216695,
+          "rotation": 0,
+          "target": "5-komunikacja---pitro"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-pokj---pitro",
+      "name": "Pokój - piętro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.276486164412672,
+          "pitch": 0.04303259123268255,
+          "rotation": 0,
+          "target": "5-komunikacja---pitro"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "StareIganieAkacjowaF2",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
